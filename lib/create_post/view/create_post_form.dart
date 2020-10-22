f84b4db44a684ff1317dcd40ce83a5127abcfe13
Robin Naumann <constorux@gmail.com>
2020-10-22 16:44:51 +0200

    finished basic UI work on the CreatePost widget

--- lib/create_post/view/create_post_form.dart
+++ lib/create_post/view/create_post_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:signup_app/create_post/cubit/create_post_cubit.dart';
 import 'package:signup_app/util/Presets.dart';
@@ -38,54 +39,106 @@ class CreatePostForm extends StatelessWidget {
                 Container(
                   padding: EdgeInsets.all(10),
                   color: AppThemeData.colorPrimaryLight,
-                  child: Column(
+                  child: Wrap(
+                    runSpacing: 10,
                     children: [
                       TextField(
                         controller: _nameController,
                         decoration:
                             Presets.getTextFieldDecoration(hintText: "Titel"),
                       ),
+                      // TODO change this to a 'chip'-style input
+                      new TextFormField(
+                        maxLines: null,
+                        style: TextStyle(color: Colors.white),
+                        decoration: Presets.getTextFieldDecoration(
+                            hintText: "Tags / Kategorien",
+                            fillColor: AppThemeData.colorBlackTrans,
+                            hintColor: AppThemeData.colorCard),
+                      ),
                     ],
                   ),
                 ),
-                Container(
-                    padding: EdgeInsets.all(10),
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(bottom: 10),
-                          child: TextField(
-                            controller: _nameController,
-                            decoration: Presets.getTextFieldDecoration(
-                                hintText: "Title"),
-                          ),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        child: Wrap(
+                          runSpacing: 10,
+                          children: [
+                            Row(
+                              children: [
+                                IconButton(
+                                  icon: Icon(Icons.calendar_today),
+                                  tooltip: 'Tap to open date picker',
+                                  onPressed: () {
+                                    Scaffold.of(context).showSnackBar(SnackBar(
+                                        content: Text("TODO: DateSelector")));
+                                  },
+                                ),
+                                Text("Datum des Events")
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                IconButton(
+                                  icon: Icon(Icons.access_time),
+                                  tooltip: 'Tap to open date picker',
+                                  onPressed: () {
+                                    Scaffold.of(context).showSnackBar(SnackBar(
+                                        content: Text("TODO: TimeSelector")));
+                                  },
+                                ),
+                                Text("Startzeit des Events")
+                              ],
+                            ),
+                            new TextFormField(
+                              decoration: Presets.getTextFieldDecoration(
+                                  hintText: "Treffpunkt"),
+                            ),
+                            new TextFormField(
+                              keyboardType: TextInputType.number,
+                              inputFormatters: <TextInputFormatter>[
+                                FilteringTextInputFormatter.digitsOnly
+                              ],
+                              decoration: Presets.getTextFieldDecoration(
+                                  hintText: "Kosten"),
+                            ),
+                            new TextFormField(
+                              minLines: 3,
+                              maxLines: null,
+                              decoration: Presets.getTextFieldDecoration(
+                                  hintText: "Text"),
+                            ),
+                          ],
                         ),
-                        Padding(
-                          padding: EdgeInsets.only(bottom: 10),
-                          child: TextField(
-                            controller: _nameController,
-                            decoration: Presets.getTextFieldDecoration(
-                                hintText: "Title"),
-                          ),
+                      ),
+                    ],
+                  ),
+                ),
+                // !TODO this might be better as a FloatingActionButton. Will change later
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Ink(
+                        decoration: const ShapeDecoration(
+                          color: AppThemeData.colorAccent,
+                          shape: CircleBorder(),
                         ),
-                        Padding(
-                          padding: EdgeInsets.only(bottom: 10),
-                          child: TextField(
-                            controller: _nameController,
-                            decoration: Presets.getTextFieldDecoration(
-                                hintText: "Title"),
-                          ),
+                        child: IconButton(
+                          tooltip: "Absenden",
+                          icon: Icon(Icons.check),
+                          color: Colors.white,
+                          onPressed: () {
+                            Scaffold.of(context).showSnackBar(SnackBar(
+                                content: Text("TODO: Post veröffentlichen")));
+                          },
                         ),
-                      ],
-                    )),
-                IconButton(
-                  splashColor: AppThemeData.colorPrimary,
-                  onPressed: () {
-                    BlocProvider.of<CreatePostCubit>(context).submitted();
-                  },
-                  icon: Icon(
-                    Icons.add,
-                    color: Colors.white,
+                      ),
+                    ],
                   ),
                 ),
               ],
