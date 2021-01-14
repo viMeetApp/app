b91f07c126cd68ce7bcb595a01aa279ad1c57a02
Tim Lindenau <tim.lindenau@outlook.de>
2021-01-14 16:56:51 +0100

    Rebuilt members of group widget. From now on Widget itself is responsible to properly rebuild. This reduces number of Network calls als minimizes the time waiting for remote data. This is the case because the old data is saved and only new data is added to the Stream instead of rebuilding every time from scratch.

---
+++ lib/widgets/group/group_settings/widgets/members_of_group_widget/cubit/members_of_group_widget_cubit.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:signup_app/repositories/group_repository.dart';
+import 'package:signup_app/util/data_models.dart';
+
+part 'members_of_group_widget_state.dart';
+
+class MembersOfGroupWidgetCubit extends Cubit<List<User>> {
+  final _groupRepository = new GroupRepository();
+  StreamSubscription _streamSubscription;
+  Stream<List<User>> currentStream;
+  MembersOfGroupWidgetCubit({@required Group group}) : super([]) {
+    updateStreamSubscription(group);
+  }
+
+  //Needs to be called when grou.users Changes
+  //Then this function cancels old (invalid) stream subscription
+  //and renews subscription with a new one
+  void updateStreamSubscription(Group group) async {
+    if (_streamSubscription != null) {
+      await _streamSubscription.cancel();
+    }
+    if (group.users.length == 0) {
+      emit([]);
+    } else {
+      currentStream =
+          _groupRepository.getStreamOfUsersWhichAreCurrentMemberOfGroup(group);
+      _streamSubscription = currentStream.listen((List<User> users) {
+        emit(users);
+      });
+    }
+  }
+}
