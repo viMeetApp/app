6106bb3cb101fd2517948ddc23a0f675c0658f36
Robin Naumann <constorux@gmail.com>
2021-01-20 10:55:07 +0100

    fixe merge sideeffects

---
+++ lib/widgets/post_detailed/cubit/post_detailed_state.dart
@@ -0,0 +1,77 @@
+part of 'post_detailed_cubit.dart';
+
+///State for the Detail Screen
+///[isFavourite] True if user liked this Event -> heart is active
+///[isSubscribed] True if user added this Event to his Events
+///[canSubsribe] True if there is a free place in the Post
+@immutable
+class PostDetailedState {
+  Post post;
+  bool isFavourite;
+  bool isExpanded = true;
+  bool showPostVerlaengern = false;
+
+  //Empty Constructor
+  PostDetailedState.empty()
+      : post = null,
+        isFavourite = false,
+        showPostVerlaengern = null,
+        isExpanded = true;
+
+  PostDetailedState({@required this.post, this.isFavourite, this.isExpanded}) {
+    //Diese Werte kann man nacher dynamisch bei erzeugung berechnen
+    this.isFavourite = this.isFavourite != null ? this.isFavourite : false;
+    this.isExpanded = this.isExpanded != null ? this.isExpanded : true;
+
+    if (post.expireDate < DateTime.now().millisecondsSinceEpoch + 86400000)
+      showPostVerlaengern = true;
+  }
+
+  PostDetailedState copyWith({
+    Post post,
+    bool isFavourite,
+    bool isExpanded,
+  }) {
+    return PostDetailedState(
+      post: post ?? this.post,
+      isFavourite: isFavourite ?? this.isFavourite,
+      isExpanded: isExpanded ?? this.isExpanded,
+    );
+  }
+}
+
+class Uninitialized extends PostDetailedState {
+  Uninitialized() : super.empty();
+}
+
+///State in which Screen is if Post is an Event
+class EventState extends PostDetailedState {
+  bool isSubscribed;
+  bool canSubscribe;
+
+  EventState({@required Post post, bool isFavourite, bool isExpanded})
+      : super(post: post, isFavourite: isFavourite, isExpanded: isExpanded) {
+    if ((post as Event)
+        .participants
+        .contains(FirebaseAuth.instance.currentUser.uid)) {
+      isSubscribed = true;
+      canSubscribe = false;
+    } else {
+      isSubscribed = false;
+      canSubscribe = (post as Event).maxPeople == -1 ||
+          (post as Event).participants.length < (post as Event).maxPeople;
+    }
+  }
+
+  EventState copyWith({Post post, bool isFavourite, bool isExpanded}) {
+    return EventState(
+        post: post ?? this.post,
+        isFavourite: isFavourite ?? this.isFavourite,
+        isExpanded: isExpanded ?? this.isExpanded);
+  }
+}
+
+///State in which Screen is if Post is a Buddy Post
+class BuddyState extends PostDetailedState {
+  BuddyState({@required Post post}) : super(post: post);
+}
--- lib/widgets/post_list/view/widgets/post_tile.dart
+++ lib/widgets/post_list/view/widgets/post_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:signup_app/widgets/post/view/post_page.dart';
 import 'package:signup_app/widgets/post_detailed/view/post_detailed_page.dart';
 import 'package:signup_app/util/data_models.dart';
 import 'package:signup_app/util/presets.dart';
