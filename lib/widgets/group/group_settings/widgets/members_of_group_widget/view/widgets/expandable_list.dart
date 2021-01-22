ffff136a6cf888cba610141de1a23157f6b39252
Tim Lindenau <tim.lindenau@outlook.de>
2021-01-22 22:07:22 +0100

    Improved group member Tile. Now the icons change depending on if user is admin

--- lib/widgets/group/group_settings/widgets/members_of_group_widget/view/widgets/expandable_list.dart
+++ lib/widgets/group/group_settings/widgets/members_of_group_widget/view/widgets/expandable_list.dart
@@ -1,13 +1,17 @@
 import 'dart:math';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:signup_app/util/data_models.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:signup_app/util/data_models.dart' as models;
 import 'package:signup_app/util/presets.dart';
+import 'package:signup_app/widgets/group/group_settings/cubit/group_settings_cubit.dart';
+import 'package:signup_app/widgets/group/group_settings/widgets/members_of_group_widget/view/widgets/member_tile.dart';
 
 ///This renders the expandable List of users
 ///It is necessary to be stateful
 class ExpandableList extends StatefulWidget {
-  final List<User> members;
+  final List<models.User> members;
   ExpandableList({@required this.members});
   @override
   _ExpandableListState createState() => _ExpandableListState();
@@ -18,34 +22,18 @@ class _ExpandableListState extends State<ExpandableList> {
   final int usersNotExpanded =
       7; //Limit of users to display in not expanded format
 
-  List<Widget> createListTiles(length, List<User> users) {
+  List<Widget> createListTiles(length, List<models.User> users, isAdmin) {
     List<Widget> widgets = [];
     //Iterarte over all users (only those to be displayed)
     //For every user append a user Tile to widgets
     for (int i = 0; i < length; ++i) {
       widgets.add(
         Padding(
-          padding: EdgeInsets.only(left: 3),
-          child: ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text(users[i].name),
-              trailing: Wrap(children: [
-                IconButton(
-                    icon: Icon(Icons.verified_user),
-                    padding: EdgeInsets.only(left: 10),
-                    onPressed: () {
-                      Scaffold.of(context).showSnackBar(
-                          SnackBar(content: Text("TODO: modify admins")));
-                    }),
-                IconButton(
-                    icon: Icon(Icons.close),
-                    padding: EdgeInsets.only(left: 10),
-                    onPressed: () {
-                      Scaffold.of(context).showSnackBar(
-                          SnackBar(content: Text("TODO: remove user")));
-                    }),
-              ])),
-        ),
+            padding: EdgeInsets.only(left: 3),
+            child: MemberTile(
+              user: users[i],
+              userHasAdminRights: isAdmin,
+            )),
       );
     }
     return widgets;
@@ -73,6 +61,12 @@ class _ExpandableListState extends State<ExpandableList> {
 
   @override
   Widget build(BuildContext context) {
+    final bool currentUserIsAdminForThisGroup =
+        BlocProvider.of<GroupSettingsCubit>(context)
+            .state
+            .group
+            .admins
+            .contains(FirebaseAuth.instance.currentUser.uid);
     //Length of list currently displayed
     int listLength = isExpanded
         ? widget.members.length
@@ -83,7 +77,8 @@ class _ExpandableListState extends State<ExpandableList> {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          ...createListTiles(listLength, widget.members),
+          ...createListTiles(
+              listLength, widget.members, currentUserIsAdminForThisGroup),
           if (shouldShowExpandedButton(listLength)) showExpandButton()
         ],
       ),
---
+++ lib/widgets/group/group_settings/widgets/members_of_group_widget/view/widgets/member_tile.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:signup_app/util/data_models.dart';
+import 'package:signup_app/widgets/group/group_settings/cubit/group_settings_cubit.dart';
+import 'package:signup_app/widgets/settings/cubit/settings_cubit.dart';
+
+class MemberTile extends StatelessWidget {
+  final User user;
+  final bool userHasAdminRights;
+
+  MemberTile({@required this.user, this.userHasAdminRights});
+  Widget build(BuildContext context) {
+    final isAdmin = BlocProvider.of<GroupSettingsCubit>(context)
+        .state
+        .group
+        .admins
+        .contains(user.id);
+    return ListTile(
+      leading: Icon(Icons.account_circle),
+      title: Text(user.name),
+      trailing: Wrap(
+        children: [
+          if (isAdmin)
+            IconButton(
+                icon: Icon(Icons.verified_user),
+                padding: EdgeInsets.only(left: 10),
+                onPressed: () {
+                  Scaffold.of(context).showSnackBar(
+                      SnackBar(content: Text("TODO: modify admins")));
+                }),
+          if (userHasAdminRights)
+            IconButton(
+                icon: Icon(Icons.close),
+                padding: EdgeInsets.only(left: 10),
+                onPressed: () {
+                  Scaffold.of(context).showSnackBar(
+                      SnackBar(content: Text("TODO: remove user")));
+                }),
+        ],
+      ),
+    );
+  }
+}
