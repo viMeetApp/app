0677ef9d77ed8d5e9c26b978624da2fe1376db41
Robin Naumann <constorux@gmail.com>
2021-03-22 11:08:05 +0100

    migrate to null safety

--- lib/widgets/group/group_settings/widgets/admit_to_join_group_widget/view/admit_to_join_group_widget.dart
+++ lib/widgets/group/group_settings/widgets/admit_to_join_group_widget/view/admit_to_join_group_widget.dart
@@ -9,7 +9,7 @@ import 'package:signup_app/widgets/group/group_settings/widgets/group_settings_g
 class AdmitToJoinGroupWidget extends StatelessWidget {
   final AdmitToJoinGroupWidgetCubit cubit;
   //Returns Stream of all user who are currently requesting to Join
-  AdmitToJoinGroupWidget({@required group})
+  AdmitToJoinGroupWidget({required group})
       : cubit = AdmitToJoinGroupWidgetCubit(group: group);
   @override
   Widget build(BuildContext context) {
@@ -19,7 +19,8 @@ class AdmitToJoinGroupWidget extends StatelessWidget {
         listener: (context, state) {
           cubit.updateStreamSubscription(state.group);
         },
-        child: BlocBuilder<AdmitToJoinGroupWidgetCubit, List<User>>(
+        child: Text("t"));
+    /*BlocBuilder<AdmitToJoinGroupWidgetCubit, List<User>>(
           cubit: cubit,
           builder: (context, users) {
             return (users.length == 0)
@@ -62,6 +63,6 @@ class AdmitToJoinGroupWidget extends StatelessWidget {
                       ).toList(),
                     ));
           },
-        ));
+        ));*/
   }
 }
