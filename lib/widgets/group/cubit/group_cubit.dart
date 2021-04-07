4be86fcc6b799bd3362e7b5c6b73481e68f69edc
Tim Lindenau <tim.lindenau@outlook.de>
2021-04-07 09:35:21 +0200

    ported group cubit to new models

--- lib/widgets/group/cubit/group_cubit.dart
+++ lib/widgets/group/cubit/group_cubit.dart
@@ -1,10 +1,10 @@
 import 'package:bloc/bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:meta/meta.dart';
 import 'package:signup_app/repositories/group_interactions.dart';
 import 'package:signup_app/repositories/user_repository.dart';
 import 'package:signup_app/util/models/data_models.dart';
+import 'package:collection/collection.dart';
 
 part 'group_state.dart';
 
@@ -36,12 +36,14 @@ class GroupCubit extends Cubit<GroupState> {
   }
 
   void _checkAndEmitGroupState(Group group) {
-    if (group.admins!.contains(userId)) {
-      emit(GroupAdmin(group: group));
-    } else if (group.users!.contains(userId)) {
-      emit(GroupMember(group: group));
-    } else {
-      emit(NotGroupMember(group: group));
+    GroupUserReference? selfReference =
+        group.members.firstWhereOrNull((member) => member.id == userId);
+    if (selfReference != null) {
+      if (selfReference.isAdmin) {
+        return emit(GroupAdmin(group: group));
+      }
+      return emit(GroupMember(group: group));
     }
+    return emit(NotGroupMember(group: group));
   }
 }
